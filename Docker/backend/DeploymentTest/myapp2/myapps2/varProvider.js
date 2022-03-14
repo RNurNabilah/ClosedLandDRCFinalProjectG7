143974fd8fdd2f42b2f55ee760ea6742733f2fc9
Raven Thevar <raven_genius@live.com>
2022-03-14 10:01:19 +0800

    Mechanism deployed to get containers to start talking.

--- Docker/backend/DeploymentTest/myapp2/myapps2/varProvider.js
+++ Docker/backend/DeploymentTest/myapp2/myapps2/varProvider.js
@@ -1,8 +1,16 @@
-function WeProvide() {
-  console.log("LOL");
-  console.log("you have beeen called");
-  const dt1 = "Hello";
-  return dt1;
-}
+const express = require("express");
+const App = express();
+const Redis = require("ioredis");
+const redis = new Redis(6379, "redis");
+//const Person = require("./person");
 
-WeProvide();
+App.use(express.json());
+
+App.get("", async (req, res) => {
+  let hi = "HELLO";
+  res.send(hi);
+});
+
+App.listen(5454, () => {
+  console.log(`Server Listening at http://provider:5454`);
+});
